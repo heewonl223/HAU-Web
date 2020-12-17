18e806e590ba95052bc9dad372658a9c980792cd
Yeonhui-ili <71022583+Yeonhui-ili@users.noreply.github.com>
2020-12-17 14:42:06 +0900

    오타 수정

--- src/components/SubNavigation.js
+++ src/components/SubNavigation.js
@@ -5,7 +5,7 @@ import {Link} from  "react-router-dom";
 const SubNavigation = () => (<nav>
     <div>
         <ul className="resultSort">
-            <li><Link id = "Nav2" to ="/diagnosis">Dianosis Result</Link></li>
+            <li><Link id = "Nav2" to ="/diagnosis">Diagnosis Result</Link></li>
             <li><Link id = "Nav2" to ="/diagnosis2">Photo</Link></li>
             <li><Link id = "Nav2" to ="/diagnosis3">Prescription</Link></li>
         </ul>
